49451c91432c15bcb4d37542c0bd15e9c9a45b81
Pavel_Sorokin <sorokin.zxcv@gmail.com>
2023-02-13 15:20:30 +0400

    Второй коммит. Начало лабораторной.

--- src/main/java/ru/ulstu/is/sbapp/SbappApplication.java
+++ src/main/java/ru/ulstu/is/sbapp/SbappApplication.java
@@ -2,12 +2,20 @@ package ru.ulstu.is.sbapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SbappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbappApplication.class, args);
 	}
-
+	@GetMapping("/String")
+	public String Vvod(@RequestParam(value = "str", defaultValue = "none") String str) {
+		return String.format("Вы ввели %s!", str);
+	}
 }
+
